349054aafa865e5eb687a38163949f3d0d4ae2d7
Burgishot175 <deyan.petkov22@gmail.com>
2026-02-05 10:09:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,12 @@ if(answer2 == "изток"):
   st.write("Правилно")
 else:
   st.write("Неправилно")
+st.write("Какъв цвят е небето")
+answer3 = st.text_input("Въведи отговор")
+if(answer3 == "синьо"):
+  st.write("Правилно")
+else:
+  st.write("Неправилно")
+
+if(answer1 == true and answer2 == true and answer3 == true):
+  st.write("Всичко ти е правилно")
