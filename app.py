afb5363aac325bac36fee5ac65d018559c3d24de
Burgishot175 <deyan.petkov22@gmail.com>
2026-02-12 10:24:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ st.write("Кога е създадена България")
 answer3 = st.text_input("Въведи oтговор")
 
 st.write("Кога почва първата световна война?")
-answer4 = st.text_input("Въведи oтговор")
+answersss4 = st.text_input("Въведи oтговор")
 
 st.button("Проверка")
-if(answer1 == "1938" and answer2 == "1945" and answer3 == "681" and answer4 == "1914"):
+if(answer1 == "1938" and answer2 == "1945" and answer3 == "681" and answersss4 == "1914"):
   st.success("Всичко е правилно")
 else:
   st.error("Имаш грешка")
