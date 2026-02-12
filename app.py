5ec7bd71748a527c995010050d005ba2cd2d33ac
Burgishot175 <deyan.petkov22@gmail.com>
2026-02-12 10:20:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.write("Кога е почнала втората световна война?")
-answer1 = st.number_input("Въведи отговор")
+answer1 = st.text_input("Въведи отговор")
 
 st.write("Кога е свършила втората световна война?")
 answer2 = st.text_input("Въведи отговор")
@@ -10,7 +10,7 @@ answer3 = st.text_input("Въведи oтговор")
 
 
 st.button("Проверка")
-if(answer1 == 1938 and answer2 == "1945" and answer3 == "681"):
+if(answer1 == "1938" and answer2 == "1945" and answer3 == "681"):
   st.success("Всичко е правилно")
 else:
   st.error("Имаш грешка")
